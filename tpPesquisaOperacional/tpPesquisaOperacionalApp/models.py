7fdb92adb7488e7d6a60ea29210acfe8ba17c8ee
Arthur Abreu <arthurpatriciosousa@gmail.com>
2024-06-25 15:45:26 -0300

    criação das migrations para o banco de dados

--- tpPesquisaOperacional/tpPesquisaOperacionalApp/models.py
+++ tpPesquisaOperacional/tpPesquisaOperacionalApp/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Abrigo(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200, null=False, blank=False)
     capacidade = models.IntegerField()
-    localizacao = models.CharField(max_length=200)
+    localizacao = models.CharField(max_length=200, null=False, blank=False)
 
     def __str__(self):
         return self.nome
 
 class Desabrigado(models.Model):
-    nome = models.CharField(max_length=200)
-    localizacao_atual = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200, null=False, blank=False)
+    localizacao_atual = models.CharField(max_length=200, null=False, blank=False)
     abrigo = models.ForeignKey(Abrigo, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
