e4059caafc5e35131276515567481bdf1cace1ba
Arthur Abreu <arthurpatriciosousa@gmail.com>
2024-07-09 14:31:49 -0300

    Trabalho finalizado

--- tpPesquisaOperacional/tpPesquisaOperacional/urls.py
+++ tpPesquisaOperacional/tpPesquisaOperacional/urls.py
@@ -6,5 +6,5 @@ from tpPesquisaOperacionalApp.views import lista_abrigos, alocar_desabrigados
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lista_abrigos, name='lista_abrigos'),
-    path('alocar/', alocar_desabrigados, name='alocar_desabrigados'),
+    path('alocar/', alocar_desabrigados, name='alocar'),
 ]
