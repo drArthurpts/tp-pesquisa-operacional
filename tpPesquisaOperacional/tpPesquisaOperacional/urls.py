0717098bd1033b117181f6a33fc546172209adb3
Arthur Abreu <arthurpatriciosousa@gmail.com>
2024-07-09 14:12:17 -0300

    Exibindo as alocações

--- tpPesquisaOperacional/tpPesquisaOperacional/urls.py
+++ tpPesquisaOperacional/tpPesquisaOperacional/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from tpPesquisaOperacionalApp.views import lista_abrigos
+from tpPesquisaOperacionalApp.views import lista_abrigos, alocar_desabrigados
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lista_abrigos, name='lista_abrigos'),
+    path('alocar/', alocar_desabrigados, name='alocar_desabrigados'),
 ]
--- tpPesquisaOperacional/tpPesquisaOperacionalApp/views.py
+++ tpPesquisaOperacional/tpPesquisaOperacionalApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Abrigo
+from .models import Abrigo, Desabrigado
 from .forms import DesabrigadoForm
+import pulp
 
 def lista_abrigos(request):
     if request.method == "GET":
@@ -18,3 +19,37 @@ def lista_abrigos(request):
     else:
         return render(request, 'lista_abrigos.html', {'form': DesabrigadoForm(), 'abrigos': Abrigo.objects.all()})
 
+
+def alocar_desabrigados(request):
+    desabrigados = list(Desabrigado.objects.all())
+    abrigos = list(Abrigo.objects.all())
+
+    # Cria o problema de otimização
+    prob = pulp.LpProblem("AlocacaoDesabrigados", pulp.LpMaximize)
+
+    # Variáveis de decisão: x[i][j] = 1 se o desabrigado i for alocado ao abrigo j, 0 caso contrário
+    x = pulp.LpVariable.dicts("x", ((i.id, j.id) for i in desabrigados for j in abrigos), cat='Binary')
+
+    # Função objetivo: maximizar o número de desabrigados alocados
+    prob += pulp.lpSum(x[i.id, j.id] for i in desabrigados for j in abrigos)
+
+    # Restrição 1: Cada desabrigado pode ser alocado a no máximo um abrigo
+    for i in desabrigados:
+        prob += pulp.lpSum(x[i.id, j.id] for j in abrigos) <= 1
+
+    # Restrição 2: Capacidade dos abrigos
+    for j in abrigos:
+        prob += pulp.lpSum(x[i.id, j.id] for i in desabrigados) <= j.capacidade
+
+    # Resolve o problema
+    prob.solve()
+
+    # Verifica o status da solução
+    alocacao = {}
+    if pulp.LpStatus[prob.status] == 'Optimal':
+        for i in desabrigados:
+            for j in abrigos:
+                if pulp.value(x[i.id, j.id]) == 1:
+                    alocacao[i.nome] = j.nome
+
+    return render(request, 'alocacao.html', {'alocacao': alocacao})
